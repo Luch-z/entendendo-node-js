46268b192971bad98cfc7f44baaa4a9d37facf6a
Luch-z <ricardopacja@gmail.com>
2025-02-03 17:03:09 -0300

    callbacks added

---
+++ 7.callbacks/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+function person(name, callback) {
+    console.log(`Hello ${name}`);
+    callback();
+}
+
+function address() {
+    console.log("India");
+}
+
+person("Sangan", address);
+
+fs.readFile('input.txt', "utf8", (err, data) => {
+    if (err) {
+        console.error("Error reading file", err);
+        return;
+    }
+
+    console.log(data);
+});
